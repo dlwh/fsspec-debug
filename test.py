20fadd92dd7a2c19a283382854dc83202182a1c6
David Hall <dlwh@stanford.edu>
2022-07-20 15:23:47 -0700

    better docs

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import fsspec
 
 total = 0
 
+# this "url chaining" string means to open data.tgz and then use the libarchive protocal to extract all files matching data/*.txt
 for f in fsspec.open_files('libarchive://data/*.txt::data.tgz', 'r'): 
    total += len(f.open().read())
 
